e17b7ad2bf19a73624226eca65ecd58d1d143507
Julian <julian.blazek@outlook.de>
2019-05-28 11:16:43 +0200

    fix and add changes

--- Woche_2/LIN_LIST.c
+++ Woche_2/LIN_LIST.c
@@ -10,20 +10,9 @@ typedef struct lin_list {
 
 LinList_p LinListAllocCell(char* payload) {
     LinListCell *ptr = (LinListCell*) malloc(sizeof(LinListCell));
-    ptr->payload = payload;
+    ptr->payload = strdup(payload);
+    ptr->next = NULL;
     return ptr;
-}
-
-void LinListFreeCell(LinList_p junk) {
-    free(junk);
-    return;
-}
-
-void LinListFree(LinList_p *junk) {
-    LinListFree(&(*junk)->next);
-    free(*junk);
-    *junk = NULL;
-    return;
 }
 
 LinList_p LinListInsertFirst(LinList_p *anchor, LinList_p newcell) {
@@ -42,10 +31,41 @@ LinList_p LinListExtractFirst(LinList_p *anchor) {
     return NULL;
 }
 
+void LinListRevert(LinList_p *anchor) {
+    LinList_p newlist = NULL;
+    LinList_p handle = NULL;
+    while(*anchor) {
+        handle = LinListExtractFirst(anchor);
+        LinListInsertFirst(&newlist, handle);
+    }
+    *anchor = newlist;
+}
+
+void LinListPrint(LinList_p list) {
+    while(list) {
+        printf("\n%s", list->payload);
+        list = list->next;
+    }
+}
+
+void LinListFreeCell(LinList_p junk) {
+    free(junk);
+    return;
+}
+
+void LinListFree(LinList_p *junk) {
+    LinListFree(&(*junk)->next);
+    free(*junk);
+    *junk = NULL;
+    return;
+}
+
 int main(int argc, char const *argv[]) {
     LinList_p list = LinListAllocCell("ab");
     LinListInsertFirst(&list, LinListAllocCell("cd"));
     LinListInsertFirst(&list, LinListAllocCell("ef"));
     printf("%x", list);
+    LinListPrint(list);
+    getchar();
     return 0;
 }
